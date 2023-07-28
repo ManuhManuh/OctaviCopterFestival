c846654e4e6e6534d7651f27807140c23493056e
ManuhManuh <cara.sterling200@gmail.com>
2023-07-27 22:49:11 -0600

    Flying up and down
    Lever will now fly player up and down; looks wobbly, but functions (can't seem to make joint strong enough) WIP

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -8,7 +8,7 @@ public class Fly : MonoBehaviour
 {
     public bool Flying => Flying;
 
-    [SerializeField] GameObject player;
+    [SerializeField] GameObject UFO;
     [SerializeField] float speed;
     [SerializeField] HingeJoint hinge;
 
@@ -28,11 +28,13 @@ public class Fly : MonoBehaviour
         {
             if(hinge.angle < 0)
             {
-                FlyUp();
+                 FlyUp();
+                Debug.Log("Flying up!");
             }
             else
             {
-                FlyDown();
+                 FlyDown();
+                Debug.Log("Flying down!");
             }
         }
     }
@@ -40,25 +42,34 @@ public class Fly : MonoBehaviour
     public void FlyUp()
     {
         // Fly up
-        player.transform.Translate(Vector3.up * speed * Time.deltaTime);
-        gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        UFO.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        //gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 
     public void FlyDown()
     {
         // Fly down
-        player.transform.Translate(Vector3.down * speed * Time.deltaTime);
-        gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        UFO.transform.Translate(Vector3.down * speed * Time.deltaTime);
+        //gameObject.transform.Translate(Vector3.up * speed * Time.deltaTime);
     }
 
     public void OnStickHeld()
     {
 
         flying = true;
+
+        // remove constraints on the stick so it can come along
+        //hinge.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationY | 
+        //                                                         RigidbodyConstraints.FreezeRotationZ;
     }
 
     public void OnStickReleased()
     {
         flying = false;
+
+        // replace stick constraints
+        //hinge.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY | 
+        //                                                         RigidbodyConstraints.FreezeRotationY | 
+        //                                                         RigidbodyConstraints.FreezeRotationZ;
     }
 }
